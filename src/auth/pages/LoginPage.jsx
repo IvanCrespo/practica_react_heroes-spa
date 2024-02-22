c0cff72f431dddb8176f002081349614d2635499
Ivan Crespo <contacto.ivancrespo@gmail.com>
2024-02-22 17:36:08 -0600

    Rutas publicas y privadas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
+
+  // Context
+  const { login } = useContext(AuthContext)
 
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/', {
+    const lastPath = localStorage.getItem('lastPath') || '/';
+    login('Ivan Crespo');
+    navigate(lastPath, {
       replace: true
     })
   }
